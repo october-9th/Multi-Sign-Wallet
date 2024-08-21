80d18f7af63d62989610797ee384b3f1348cb238
arthur <viet121240@gmail.com>
2024-08-22 03:20:00 +0700

    final multi signature wallet

--- src/MultiSigWallet.sol
+++ src/MultiSigWallet.sol
@@ -15,28 +15,216 @@ pragma solidity ^0.8.24;
 
 contract MultiSigWallet {
     // list of owners
-    address[] public owners;
+    address[] private owners;
 
     // check if address is already owner
     mapping(address => bool) public isOwner;
 
     // number of approvaols required
-    uint256 public numberOfApprovs;
+    uint256 public numberOfApproveRequired;
 
-    struct Transactions {
+    struct Transaction {
         address to;
         uint256 amount;
         bool executed;
+        uint256 numberOfApprovals;
+        bytes data;
     }
 
-    Transactions[] public transactionsList;
-    mapping(uint256 => mapping(address => bool)) transactionsApproval;
+    Transaction[] public transactionsList;
 
-    constructor(address[] memory _owner, uint256 _numberOfApprovs) {
-        require(_owner.length > 0, "list of owner must not be empty");
+    // mapping from txIndex => owner => true|false
+    mapping(uint256 => mapping(address => bool)) private transactionsApproval;
+
+    // event *
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 indexed txIndex,
+        uint256 amount,
+        bytes data
+    );
+
+    event Deposit(address indexed sender, uint256 amount, uint256 balance);
+
+    event ApproveTransaction(address indexed sender, uint256 txIdex);
+
+    event RevokeTransaction(address indexed sender, uint256 txIdex);
+
+    event executingTransaction(address indexed sender, uint256 txIndex);
+    // modifier *
+
+    // only owner allow to call function
+    modifier onlyOnwer() {
+        require(isOwner[msg.sender], "you are not owner");
+        _;
+    }
+
+    // transaction exists
+    modifier txExist(uint256 txIndex) {
+        require(txIndex < transactionsList.length, "transaction don't exist");
+        _;
+    }
+
+    // transaction status
+    modifier txNotExecuted(uint256 txIndex) {
         require(
-            _numberOfApprovs > 0 && _numberOfApprovs <= _owner.length,
-            "number of approvals must not be greater than number of owners"
+            !transactionsList[txIndex].executed,
+            "transaction already executed"
+        );
+        _;
+    }
+
+    // transaction approval
+    modifier txNotApproved(uint256 txIndex) {
+        require(
+            !transactionsApproval[txIndex][msg.sender],
+            "transaction already approved"
+        );
+        _;
+    }
+
+    constructor(address[] memory _owners, uint256 _numberOfApproveRequired) {
+        require(_owners.length > 0, "list of owner must not be empty");
+        require(
+            _numberOfApproveRequired > 0 &&
+                _numberOfApproveRequired <= _owners.length,
+            "number of approval required must not be greater than number of owners"
+        );
+
+        for (uint256 i = 0; i < _owners.length; i++) {
+            address owner = _owners[i];
+            require(_owners[i] != address(0), "invalid address");
+            require(!isOwner[owner], "address already exists as an owner");
+
+            isOwner[owner] = true;
+            owners.push(owner);
+        }
+
+        numberOfApproveRequired = _numberOfApproveRequired;
+    }
+
+    // receive function
+    receive() external payable {}
+
+    // transaction submission
+    function submitTransaction(
+        address _to,
+        uint256 _amount,
+        bytes calldata _data
+    ) public onlyOnwer {
+        require(_to != address(0), "invalid address");
+        require(_amount > 0, "amount ethers to sent must be greater than 0");
+
+        // get the index of transaction
+        uint256 _txIndex = transactionsList.length;
+        transactionsList.push(
+            Transaction({
+                to: _to,
+                amount: _amount,
+                executed: false,
+                numberOfApprovals: 0,
+                data: _data
+            })
+        );
+        emit Transfer(msg.sender, _to, _txIndex, _amount, _data);
+    }
+
+    // confirm transaction
+    function approveTransaction(
+        uint256 _txIndex
+    )
+        public
+        onlyOnwer
+        txExist(_txIndex)
+        txNotApproved(_txIndex)
+        txNotExecuted(_txIndex)
+    {
+        // increase the number of approval for this transaction
+        transactionsList[_txIndex].numberOfApprovals++;
+        // approve this transaction
+        transactionsApproval[_txIndex][msg.sender] = true;
+
+        // emit event
+        emit ApproveTransaction(msg.sender, _txIndex);
+    }
+
+    // revoke transaction
+    function revokeTransaction(
+        uint256 _txIndex
+    ) public onlyOnwer txExist(_txIndex) txNotExecuted(_txIndex) {
+        // require this transaction to be already approved
+        require(
+            transactionsApproval[_txIndex][msg.sender],
+            "transaction is not approved"
+        );
+
+        // decrease the number of approval for this transaction
+        transactionsList[_txIndex].numberOfApprovals--;
+
+        // revoke transaction
+        transactionsApproval[_txIndex][msg.sender] = false;
+
+        // emit event for revoke
+        emit RevokeTransaction(msg.sender, _txIndex);
+    }
+
+    // execute a transaction
+    function executeTransaction(
+        uint256 _txIndex
+    ) public onlyOnwer txExist(_txIndex) txNotExecuted(_txIndex) {
+        // to be in the state of execution, this transaction must be already approved
+        require(
+            transactionsApproval[_txIndex][msg.sender],
+            "transaction is not approved yet"
+        );
+
+        // the number of approval for this transaction must be greater than the number of approval required by the network
+        require(
+            transactionsList[_txIndex].numberOfApprovals >=
+                numberOfApproveRequired,
+            "number of approval for this transaction is not satisfied by the network"
+        );
+
+        // if the number of approval required by the network is alreay satisfied, execute the transaction
+        Transaction memory _tx = transactionsList[_txIndex];
+        _tx.executed = true;
+
+        // exeucte the transaction
+        (bool status, ) = _tx.to.call{value: _tx.amount}(_tx.data);
+        require(status, "tx failed");
+
+        emit executingTransaction(msg.sender, _txIndex);
+    }
+
+    function getOwners() public view returns (address[] memory) {
+        return owners;
+    }
+
+    function countTransaction() public view returns (uint256) {
+        return transactionsList.length;
+    }
+
+    function getTransaction(
+        uint256 _txIndex
+    )
+        public
+        view
+        returns (
+            address to,
+            uint256 amount,
+            bytes memory data,
+            bool executed,
+            uint256 numberOfApprovals
+        )
+    {
+        Transaction memory _tx = transactionsList[_txIndex];
+        return (
+            _tx.to,
+            _tx.amount,
+            _tx.data,
+            _tx.executed,
+            _tx.numberOfApprovals
         );
     }
 }
